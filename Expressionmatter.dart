9cd161254a41099d34549f56914bfb7dda48cfbf
Xuso18 <148717389+Ahs301@users.noreply.github.com>
2025-11-12 12:09:44 +0100

    Update Expressionmatter.dart

--- Expressionmatter.dart
+++ Expressionmatter.dart
@@ -11,5 +11,6 @@ int expressionMatter(int a, int b, int c) {
     a * (b + c),
     (a + b) * c,
     a * b + c
-  ].reduce((curr, next) => curr > next ? curr : next);
+  ].reduce((curr, next) => curr > next ? curr : next); //recorre el array y compara los valores para quedarse con el mayor de todos.
+  //curr > next ? curr : next significa: “Si curr es mayor que next, quédate con curr; si no, con next”.
 }
